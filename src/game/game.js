811e34a3aa06527cab6490c8e877c2a1d4885ab9
Lorenzo Sauvaire <lorenzo.sauvaire@epitech.eu>
2019-04-28 22:04:05 +0200

    game.js

---
+++ src/game/game.js
@@ -0,0 +1,20 @@
+function gameDraw() {
+
+    //drawing
+    pDraw.drawBackground('white');
+
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].draw();
+        if (boxes[i].offScreen()) {
+            Matter.World.remove(world, boxes[i].body);
+            boxes.splice(i, 1);
+        }
+    }
+
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].draw();
+    }
+
+    // portal.draw();
+    player.draw();
+}
--- src/main.js
+++ src/main.js
@@ -89,23 +89,8 @@ function draw() {
     Matter.Engine.update(engine);
     gameUpdate();
 
-    //drawing
-    pDraw.drawBackground('white');
-
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i].draw();
-        if (boxes[i].offScreen()) {
-            Matter.World.remove(world, boxes[i].body);
-            boxes.splice(i, 1);
-        }
-    }
-
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].draw();
-    }
-
-    // portal.draw();
-    player.draw();
+    //game
+    gameDraw();
 }
 
 function keyReleased() {
